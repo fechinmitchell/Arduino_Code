be184b5832632ccbe48885e1098d1093f250623c
Fechin Mitchell <f.mitchell3@nuigalway.ie>
2018-11-01 13:30:25 +0000

    Arduino_Code

---
+++ Demo_6_LDR_Night_Light.ino
@@ -0,0 +1,22 @@
+
+int sensePin = 0;
+int ledPin = 9;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  analogReference(DEFAULT);
+
+  pinMode(ledPin, OUTPUT);
+
+
+
+}
+
+void loop() {
+
+  int val = analogRead(sensePin);
+
+  if (val < 800) digitalWrite(ledPin, LOW);
+  else digitalWrite(ledPin, HIGH);
+}
